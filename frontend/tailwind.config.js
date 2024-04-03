45bd52404d1480a66d3b7c88665072ff0da90703
FerJuan <fbatres@gmail.com>
2024-04-03 18:22:58 -0300

    agrego colores a .config2

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        //primarios
+        'backgroud':'F5F5F5',
+        'primary':'#851291',
+        'text':'#080808',
+        //secundarios
+        'secondary':'#65006F',
+        'white':'#FFFFFF',
+        'text-disabled':'#717171',
+        // gradientes
+        //TODO! ARMAR UNA CLASE PARA USAR SOLO UN CODIGO PARA GRADIENTE, por el momento agrego "codigo-v-start" - "codigo-v-end"
+        'degraded-v-start':'#851291',
+        'degraded-v-end':'#65006F',
+        'degraded-h-start':'#851291',
+        'degraded-h-end':'#65006F',
+        //colores de alerta
+        'error':'#BD2734',
+        'error-light':'#D6525D',
+        'alert':'#DCBD4C',
+        'check':'#79B465'
+
+      }
+    },
   },
   plugins: [],
 }
