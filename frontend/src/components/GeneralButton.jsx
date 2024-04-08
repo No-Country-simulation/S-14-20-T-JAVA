d753a22806e9a319902542af24ed51d8f7f5f05c
majuarezmagarinios <majuarezmagarinios@gmail.com>
2024-04-08 16:26:09 -0300

    boton generico general creado

---
+++ frontend/src/components/GeneralButton.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+export const GeneralButton = ({ name, to }) => {
+    return (
+        <Link
+            className="flex items-center justify-center 
+                bg-[#F5F5F5]  rounded-full w-[187px]  h-[39px] shadow-[-5px_1px_4px_0px_#00000038]"
+            to={to}
+        >
+            {name}
+        </Link>
+    );
+};
