67afb891a522daeaa2a2fb5e0fb3961e3b63aa7a
Ferjuan <fbatres@gmail.com>
2024-04-10 20:37:23 -0300

    se trae info de pexels y se guarda en useEffect

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,8 +1,33 @@
 
+import fetchPhotos from "./resources/pexels"
+ import { useEffect, useState } from "react"
 const Post = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPhotos();
+        setPhotos(data);
+      } catch (error) {
+        console.error('Error fetching photos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Post</div>
-  )
+    <>
+    {
+      photos.map((picture) => (
+        <div key={picture.id}>
+        <img src={picture.src.large} alt={picture.alt} />
+        </div>
+      ))
+    }
+    </>
+  );
 }
 
 export default Post
--- frontend/src/components/resources/pexels.js
+++ frontend/src/components/resources/pexels.js
@@ -1,17 +1,15 @@
-
 import { createClient } from 'pexels';
 
 const client = createClient('kXjh4hyuM6hRDlZGG1abZfFUKq96oOtAWpm1H2xJq2ZCUwuyljqmEG6X');
-const query = 'Nature';
 
+const fetchPhotos = async (query = 'Nature', perPage = 10) => {
+  try {
+    const response = await client.photos.search({ query, per_page: perPage });
+    return response.photos;
+  } catch (error) {
+    console.error('Error fetching photos from Pexels:', error);
+    throw error;
+  }
+};
 
-client.photos.search({ query, per_page: 10 }).then(photos => {
-    console.log(photos);
-    const photo = photos.photos[0]
-    photo
-    const image = photo.map(photo => {
-        id = photo.id
-        src = photo.src.large
-    })
-    console.log(image)
-});
+export default fetchPhotos;
