6c15ab475b2be73a4dc1491bdf0e2ad55e4e3d85
verocampero <verocampero2@gmail.com>
2024-04-09 22:54:39 -0300

    pantallas verificar contraseña + cambiar contraseña

--- frontend/src/components/ButtonGeneric.jsx
+++ frontend/src/components/ButtonGeneric.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ButtonGeneric({bgColor,text,ColorText,Shadow}) {
   return (
- <button type="button" className={`rounded-full w-44 p-3 ${bgColor} ${ColorText} ${Shadow}`}>
+ <button type="button" className={`rounded-full w-full w-44 p-3 ${bgColor} ${ColorText} ${Shadow}`}>
 
     {text}
  
