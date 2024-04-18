2ac94a9fb137da018f51c0924a86452279b181fa
Vero Campero <130777997+verocampero@users.noreply.github.com>
2024-04-18 05:44:11 -0300

    se incorporo la pantalla profile, componentes de la carpeta Publication (#17)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Forgottenpassword from "./views/Sesions/Forgottenpassword";
 import VerificationCode from "./views/Sesions/VeriticationCode";
 import CreatePassword from "./views/Sesions/CreatePassword";
 import { Route, Routes } from "react-router-dom";
-
+import AnotherProfile from './views/Porfile/AnotherProfile'
 
 import CreateUser from "@/views/Sesions/CreateUser";
 function App() {
@@ -16,14 +16,18 @@ function App() {
     <Layout className={forceMobileStyles}>
       <Routes>
         <Route path="/createuser" element={<CreateUser />} />
-        <Route
-          path="/"
-          element={<SplashScreen delay={3000} destination="/login" />}
-        />
+        <Route path="/" element={<SplashScreen delay={3000} destination="/login" />}/>
+
+        
         <Route path="/login" element={<Login />} />
         <Route path="/forgottenpassword" element={<Forgottenpassword />} />
         <Route path="/verificationcode" element={<VerificationCode />} />
         <Route path="/createpass" element={<CreatePassword/>} />
+
+        // descomentar para ver la pantalla profile
+        {/* <Route path="/login" element={<AnotherProfile  />} /> */}
+
+      
       </Routes>
       
     </Layout>
