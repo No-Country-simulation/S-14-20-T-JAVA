0c2f1c81ff3225d50e5bf62d9a35082f1d331b60
verocampero <verocampero2@gmail.com>
2024-04-24 15:39:09 -0300

    formulario para ser emprendedor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SplashScreen from '@views/SplashScreen';
 import Login from '@views/Login';
 import CreateUser from '@views/Sesions/CreateUser';
 import Home from '@views/Home';
-
+import SerEmprendedor from './components/FormularioEmprendedor/serEmprendedor';
 import Forgottenpassword from './views/Sesions/Forgottenpassword';
 import VerificationCode from './views/Sesions/VeriticationCode';
 import CreatePassword from './views/Sesions/CreatePassword';
@@ -13,6 +13,10 @@ import AnotherProfile from './views/Porfile/AnotherProfile';
 import Search from '@/views/SEarch/Search';
 import Reports from "./views/Reporte/Reports";
 import Report from "./views/Reporte/Report";
+import Ocupacion from './components/FormularioEmprendedor/Ocupacion';
+import TuOcupacion from './components/FormularioEmprendedor/tuOcupacion';
+import Enviado from './components/FormularioEmprendedor/Enviado'
+
 
 function App() {
     
@@ -25,7 +29,7 @@ function App() {
                     element={<SplashScreen delay={3000} destination="/login" />}
                 />
 
-                <Route path="/login" element={<Login />} />
+                {/* <Route path="/login" element={<Login />} /> */}
                 <Route
                     path="/forgottenpassword"
                     element={<Forgottenpassword />}
@@ -43,6 +47,10 @@ function App() {
                 <Route path="/profile" element={<AnotherProfile />} />
                 <Route path="/reports" element={<Reports/>}/>
                  <Route path="reports/report" element={<Report/>}/>
+              <Route path="/login" element={<SerEmprendedor />} />
+                 <Route path="/tuocupacion" element={<TuOcupacion />} />
+                 <Route path="/ocupacion" element={<Ocupacion />} />
+                 <Route path="/enviado" element={<Enviado />} />
             </Routes>
 
             
---
+++ frontend/src/components/FormularioEmprendedor/tuOcupacion.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import LogoBIzWIz from "../../components/icons/LogoBIzWIz";
+import InputComponents from "../../components/InputComponents";
+import { GeneralButton } from "../../components/GeneralButton";
+import { Link } from "react-router-dom";
+const TuOcupacion = () => {
+  return (
+    <div
+      className="h-screen bg-primary  pt-12 
+ flex flex-col items-center gap-12  justify-between "
+    >
+      <section>
+        <LogoBIzWIz color="positive" />
+      </section>
+      <div
+        className="formulario bg-white py-12 px-4 h-full
+         flex items-center flex-col w-[101%] gap-4  rounded-tr-[20%]"
+      >
+        <section
+          className="formulario bg-white py-12 px-4 h-full
+         flex items-center flex-col w-[101%] gap-4  rounded-tr-[20%]"
+        >
+          <h6 className="font-black text-xl">Tu ocupación</h6>
+
+          <p className="text-[14px] opacity-75 p-3 text-center">
+          Selecciona que tipo de ocupación tienes, asi
+podremos recomendarte a mas personas
+con respecto a sus gustos.
+          </p>
+
+          <InputComponents
+            type="text"
+            place={"Ocupación"}
+            inputName={"ocupacion"}
+            id={"ocupacion"}
+          />
+
+          {/* <ButtonGeneric
+            text={"Siguiente"}
+            bgColor={"bg-primary"}
+            ColorText={"text-white"}
+            Shadow={"shadow-lg"}
+            to={"/ocupacion"}
+          /> */}
+           <Link to={'/ocupacion'}>
+          <GeneralButton
+          type="submit"
+          name={'Siguiente'}
+    
+          
+          /></Link>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default TuOcupacion;
